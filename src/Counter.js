4d9abac32b68b66e47fe6d4f6b42bbe984468ef0
manuu07 <manusrivastava229@gmail.com>
2023-01-06 02:36:28 +0530

    assignment-3

---
+++ src/Counter.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import './counter.css';
+
+//========================Counter Using Class Component=================================================
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            count : 0
+        }
+    }
+    render() {
+        return (
+            <div className='container'>
+             <div>
+             <h1>Counter : {this.state.count}</h1>
+         </div>
+         <div>
+             <button onClick={() => this.setState({count: this.state.count+1})}>Increment +</button>
+             <button onClick={() => this.setState({count:this.state.count-1})}>Decrement -</button>
+         </div>
+         </div>
+        )
+    }
+}
+
+
+
+//========================Counter Using Functional Component=================================================
+
+// function Counter() {
+//     const [count, setCount] = useState(0)
+    
+//     return (
+//         <div className='container'>
+//             <div>
+//             <h1>Counter: {count}</h1>
+//         </div>
+//         <div>
+//             <button onClick ={ (e)=>setCount(count+1)}>Increment +</button>
+//             <button onClick ={(e) => setCount(count-1)}>Decrement -</button>
+//         </div>
+//         </div>
+//     );
+// }
+
+
+export default Counter ;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from "./Form"
+import Counter from "./Counter"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Form/>
+   <Counter/>
   </React.StrictMode>
 );
 
